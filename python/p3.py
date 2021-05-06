203b114f45f4caf384877c804de60e4d1ac274c0
brian <brian@pop-os.localdomain>
2021-05-06 19:36:30 -0400

    first try at a bad solution for problem #3

--- python/p3.py
+++ python/p3.py
@@ -3,9 +3,38 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 
+'''
+
+'''
+def is_prime( num ): #---------------------------
+    prime = True
+
+    for i in range( 2, num ):
+        if num % i == 0:
+            prime = False
+
+        if not prime:
+            return prime
+
+    return prime
+# -----------------------------------------------
 
 
 
+
+if __name__ == "__main__": #---------------------
+    num = 600851475143
+    i = num
+    done = False
+
+    while not done:
+        i -= 1
+        if num % i == 0:
+            if is_prime( i ):
+                print(i)
+                done = True
+
+# -----------------------------------------------
 
 
 
